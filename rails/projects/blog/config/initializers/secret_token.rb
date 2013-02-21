5aca5d693bd5a8b5edaacb6b0758045d38a8b2fe
Taisuke Yoshida <taiyou.y3@gmail.com>
2013-02-22 00:58:06 +0900

    blogアプリ作成：プロジェクト作成、Postオブジェクトの作成

---
+++ rails/projects/blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '8a8bd05fd8a338b20da43fce2ad0c73bbe3a8c10cdfd92a7b4e489071305aa9a54c1877b0b5cbf6f355805e0527e38b84c222ad24838fef1c1a7688108527483'
