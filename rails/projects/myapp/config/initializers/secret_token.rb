9f76555b6f0ba5aa502da9e66fe66f9d90c994fd
Taisuke Yoshida <taiyou.y3@gmail.com>
2013-02-21 23:43:39 +0900

    rails new,scaffold study

---
+++ rails/projects/myapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myapp::Application.config.secret_token = 'a95d3e31a1af88735632ea516381f7f27abe43160aa2422ca0619f039d2a04cfb16a91d3b17a3e4845ca436d36dba401e7e57151155e9f19f60f021fd31acbb9'
