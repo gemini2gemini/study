c829227595402219771505057180325304a30a53
Taisuke Yoshida <taiyou.y3@gmail.com>
2013-02-28 00:58:22 +0900

    post.rbにhas_many :commentsの記載を忘れていたので追記

--- rails/projects/blog/app/models/post.rb
+++ rails/projects/blog/app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ActiveRecord::Base
   attr_accessible :content, :title
 
+  has_many :comments
+  
   validates :title, :presence => true
   validates :content, :presence => true,
                         :length => {:minimum => 5}
