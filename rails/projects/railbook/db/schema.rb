0752f159a9a3c7fc3382fb938e2b2583acf2b495
Taisuke Yoshida <taiyou.y3@gmail.com>
2013-03-17 16:11:55 +0900

    reke db:migrateを実行 db/schema.rbに追記

--- rails/projects/railbook/db/schema.rb
+++ rails/projects/railbook/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130315151847) do
+ActiveRecord::Schema.define(:version => 20130317055138) do
 
   create_table "books", :force => true do |t|
     t.string   "isbn"
