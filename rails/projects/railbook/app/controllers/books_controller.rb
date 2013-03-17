f81f2c5b7098902db86be8b75a20f713c13ecb53
Taisuke Yoshida <taiyou.y3@gmail.com>
2013-03-17 17:29:34 +0900

    indexメソッドに respond_to  format.xmlを追記

--- rails/projects/railbook/app/controllers/books_controller.rb
+++ rails/projects/railbook/app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ class BooksController < ApplicationController
 
     respond_to do |format|
       format.html # index.html.erb
+      format.xml {render xml: @books}
       format.json { render json: @books }
     end
   end
